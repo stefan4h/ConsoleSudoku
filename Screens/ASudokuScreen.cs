9eb94adce9277aedd28e179864e4bcd1d9a0779b
Stefan Humpelstetter <shumpelstetter@gmail.com>
2022-02-07 17:57:29 +0100

    Add random color to title

--- Screens/ASudokuScreen.cs
+++ Screens/ASudokuScreen.cs
@@ -8,18 +8,23 @@ namespace ConsoleSudoku.Screens {
     public abstract class ASudokuScreen {
 
         protected const int LINE_LENGTH = 90;
+        protected ConsoleColor randomColor;
         protected const ConsoleColor defaultColor = ConsoleColor.White;
         protected const ConsoleColor selectColor = ConsoleColor.Blue;
         protected bool exit = false;
 
+        public ASudokuScreen() {
+            randomColor = GetRandomConsoleColor();
+        }
+
         private void ShowTitle() {
-            CW("   _____                           _          _____             _         _              |");
-            CW("  / ____|                         | |        / ____|           | |       | |             |");
-            CW(" | |      ___   _ __   ___   ___  | |  ___  | (___   _   _   __| |  ___  | | __ _   _    |");
-            CW(" | |     / _ \\ | '_ \\ / __| / _ \\ | | / _ \\  \\___ \\ | | | | / _` | / _ \\ | |/ /| | | |   |");
-            CW(" | |____| (_) || | | |\\__ \\| (_) || ||  __/  ____) || |_| || (_| || (_) ||   < | |_| |   |");
-            CW("  \\_____|\\___/ |_| |_||___/ \\___/ |_| \\___| |_____/  \\__,_| \\__,_| \\___/ |_|\\_\\ \\__,_|   |");
-            CW(GetStringAsRepeatedChar('_', LINE_LENGTH - 1) + "|");
+            W("   _____", randomColor); W("                           _          "); W("_____", randomColor); W("             _         _              "); W("|\n", randomColor);
+            W("  / ____|", randomColor); W("                         | |        "); W("/ ____|", randomColor); W("           | |       | |             "); W("|\n", randomColor);
+            W(" | |", randomColor); W("      ___   _ __   ___   ___  | |  ___  "); W("| (___", randomColor); W("   _   _   __| |  ___  | | __ _   _    "); W("|\n", randomColor);
+            W(" | |", randomColor); W("     / _ \\ | '_ \\ / __| / _ \\ | | / _ \\  "); W("\\___ \\", randomColor); W(" | | | | / _` | / _ \\ | |/ /| | | |   "); W("|\n", randomColor);
+            W(" | |____", randomColor); W("| (_) || | | |\\__ \\| (_) || ||  __/  "); W("____) |", randomColor); W("| |_| || (_| || (_) ||   < | |_| |   "); W("|\n", randomColor);
+            W("  \\_____|", randomColor); W("\\___/ |_| |_||___/ \\___/ |_| \\___| "); W("|_____/", randomColor); W("  \\__,_| \\__,_| \\___/ |_|\\_\\ \\__,_|   "); W("|\n", randomColor);
+            CW(GetStringAsRepeatedChar('_', LINE_LENGTH - 1) + "|", randomColor);
             CW();
             CW();
         }
@@ -49,6 +54,17 @@ namespace ConsoleSudoku.Screens {
             return sb.Append(c, count).ToString(); ;
         }
 
+        protected ConsoleColor GetRandomConsoleColor() {
+            Random rnd = new Random();
+
+            ConsoleColor color = (ConsoleColor)rnd.Next(0, Enum.GetNames(typeof(ConsoleColor)).Length);
+
+            while (color == ConsoleColor.Black)
+                color = (ConsoleColor)rnd.Next(0, Enum.GetNames(typeof(ConsoleColor)).Length);
+
+            return color;
+        }
+
         protected void ShowSmallerTitle(string title) {
             CW(GetStringAsRepeatedChar('_', title.Length));
             CW(GetStringAsRepeatedChar(' ', title.Length) + "|");
@@ -64,6 +80,7 @@ namespace ConsoleSudoku.Screens {
         public void Show() {
             Console.CursorVisible = false;
             do {
+                randomColor = GetRandomConsoleColor();
                 Console.Clear();
                 ShowTitle();
                 Draw();
