370b886d86be824bfa2606781cc13ec1ec12fd9a
Stefan Humpelstetter <shumpelstetter@gmail.com>
2022-04-29 14:52:21 +0200

    Add comments

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ namespace ConsoleSudoku {
     public class Program {
 
         static void Main(string[] args) {
-            Console.SetWindowSize(95, 40);
-            Game.Play();
+            Console.SetWindowSize(95, 40); // set windows size
+            Game.Play(); // start the game
         }
     }
 }
